95dce9715a900cfab2d7e96a09daabe0755ef621
abhi786998@gmail.com <abhi786998@gmail.com>
2024-02-29 23:03:12 +0530

    Clock Code Pushed

---
+++ Project2/Bharat-Clock/src/Componet/Paragraph.jsx
@@ -0,0 +1,9 @@
+function Paragraph(){
+    return(
+        <p>
+           This is the clock show the time in Bharat at all times 
+        </p>
+    )
+}
+
+export default Paragraph;
---
+++ Project2/Bharat-Clock/src/Currenttime.jsx
@@ -0,0 +1,11 @@
+function Currenttime() {
+
+    let time = new Date();
+    return(
+        <h1 className="lead">
+            This is the current time: {time.toLocaleDateString()} -{time.toLocaleTimeString()}
+        </h1>
+    )
+}
+
+export default Currenttime;
